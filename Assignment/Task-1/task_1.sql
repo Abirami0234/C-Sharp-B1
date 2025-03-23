0d42a2188252b9fed14b2dccb5b3003a1219d65b
Abirami0234 <96755590+Abirami0234@users.noreply.github.com>
2025-03-23 21:06:25 +0530

    Update task_1.sql

--- Assignment/Task-1/task_1.sql
+++ Assignment/Task-1/task_1.sql
@@ -1,1 +1,28 @@
-sql
+create database HMBank
+use HMBank
+
+create table customers (
+customer_id int primary key,
+first_name varchar(50) not null,
+last_name varchar(50) not null,
+dob date not null,
+email varchar(100) unique,
+phone_number varchar(15) unique,
+address varchar(255) not null
+)
+ create table accounts (
+ account_id int primary key,
+customer_id int references customers(customer_id),
+account_type varchar(20) check (account_type in ('savings', 'current', 'zero_balance')),
+balance decimal(12,2)
+ )
+
+ create table transactions(
+ transaction_id int primary key,
+ account_id int references accounts(account_id),
+ transaction_type varchar(20) check (transaction_type in ('deposit', 'withdrawal', 'transfer')),
+ amount decimal(12, 2), 
+ transaction_date date
+ )
+
+
