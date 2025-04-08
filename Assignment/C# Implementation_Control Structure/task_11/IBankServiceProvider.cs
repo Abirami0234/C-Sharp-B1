ba3e440b0a2990b9f422dccf5ffd4b04a780223c
Abirami0234 <96755590+Abirami0234@users.noreply.github.com>
2025-04-09 01:58:16 +0530

    Update IBankServiceProvider.cs

--- Assignment/C# Implementation_Control Structure/task_11/IBankServiceProvider.cs
+++ Assignment/C# Implementation_Control Structure/task_11/IBankServiceProvider.cs
@@ -3,7 +3,7 @@
     public interface IBankServiceProvider
     {
         void create_account(long accNo, string accType, float balance, task10_Customer customer);
-        task10_Account[] listAccounts(); // MUST match this return type
+        task10_Account[] listAccounts(); 
         void calculateInterest();
     }
 }
