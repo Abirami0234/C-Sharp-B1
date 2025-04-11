58e26b576c13c96636c400505d021d094e70a6aa
Abirami0234 <96755590+Abirami0234@users.noreply.github.com>
2025-04-11 10:59:25 +0530

    Update Program.cs

--- Daily Assignments/DA_2/Program.cs
+++ Daily Assignments/DA_2/Program.cs
@@ -115,20 +115,14 @@
         }
     }
 
-    // class FurtherDerived : DerivedClass
-    // {
-    //     public override void Show() ❌ Error: Cannot override sealed method
-    //     {
-    //         Console.WriteLine("Attempting to Override Sealed Method");
-    //     }
-    // }
+    
 
     class Program
     {
         static void Main()
         {
             DerivedClass obj = new DerivedClass();
-            obj.Show();  // Output: Sealed Method in Derived Class
+            obj.Show();  
         }
     }
 }
